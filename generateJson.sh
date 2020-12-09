353c6333d3d0059b70c7553baeda44be941eedea
Amine KHARROU <amine.1017@gmail.com>
2020-12-09 23:30:46 +0100

    add ID in generation script

--- generateJson.sh
+++ generateJson.sh
@@ -1,11 +1,14 @@
 # /bin/bash
 
-PREFIX_LINK="http://ddddd"
+PREFIX_LINK="https://raw.githubusercontent.com/picsoft/skins/master/"
+I=1
 
 for skin in *
 do
    echo "{";
-   echo "  uri : \"$PREFIX_LINK$skin\",";
-   echo "  name : \"${skin/.png/}\","
+   echo "  id: $I,"
+   echo "  uri: \"$PREFIX_LINK$skin\",";
+   echo "  name: \"${skin/.png/}\","
    echo "}," 
+   ((I=I+1))
 done
